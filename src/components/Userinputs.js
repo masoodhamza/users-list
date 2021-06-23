810619d637d441715fdbceec0bded30d6370e024
Hamza Masood <masood.hamzaa@gmail.com>
2021-06-23 21:01:52 +0500

    set state

--- src/components/Userinputs.js
+++ src/components/Userinputs.js
@@ -1,16 +1,53 @@
+import React, { useState } from "react";
 import Button from "./Button";
 
 const UserInputs = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  const nameChangeHandler = (e) => {
+    setName(e.target.value);
+  };
+
+  const ageChangeHandler = (e) => {
+    setAge(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (name.trim().length === 0 || age.trim().length === 0) {
+      console.log("value is not valid");
+      setIsValid(false);
+      return;
+    }
+
+    console.log(name, age);
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" />
+        <input
+          type="text"
+          name="username"
+          id="username"
+          onChange={nameChangeHandler}
+          value={name}
+        />
       </div>
 
       <div>
         <label htmlFor="age">Age</label>
-        <input type="number" name="age" id="age" />
+        <input
+          type="number"
+          name="age"
+          id="age"
+          onChange={ageChangeHandler}
+          value={age}
+        />
       </div>
 
       <Button>Add User</Button>
