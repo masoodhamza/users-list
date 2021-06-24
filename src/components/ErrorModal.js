eb2f408f0ba35231b8c7eb74cbb63aa71bec8ab4
Hamza Masood <masood.hamzaa@gmail.com>
2021-06-24 16:24:20 +0500

    error css

--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -1,11 +1,14 @@
 import Button from "./Button";
+import style from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
   return (
-    <div>
-      <h1>Invalid Input</h1>
-      <p>Please enter a valid name & age</p>
-      <Button>OK</Button>
+    <div className={style.backdrop}>
+      <div className={style.modal} onClick={props.onConfirm}>
+        <h1 className={style.title}>{props.title}</h1>
+        <p className={style.message}>{props.message}</p>
+        <Button className={style.button}>OK</Button>
+      </div>
     </div>
   );
 };
