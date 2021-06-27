dad38079c7c289984d13c20202a9c0c1d1843cb4
Hamza Masood <masood.hamzaa@gmail.com>
2021-06-27 12:19:41 +0500

    error modal render above root

--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -1,15 +1,39 @@
+import React from "react";
 import Button from "./Button";
 import style from "./ErrorModal.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={style.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={style.modal}>
+      <h1 className={style.title}>{props.title}</h1>
+      <p className={style.message}>{props.message}</p>
+      <Button>OK</Button>
+    </div>
+  );
+};
 
 const ErrorModal = (props) => {
   return (
-    <div className={style.backdrop} onClick={props.onConfirm}>
-      <div className={style.modal}>
-        <h1 className={style.title}>{props.title}</h1>
-        <p className={style.message}>{props.message}</p>
-        <Button>OK</Button>
-      </div>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("modal-root")
+      )}
+    </React.Fragment>
   );
 };
 
